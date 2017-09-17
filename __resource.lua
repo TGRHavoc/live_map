58d9bb9565a369c629f208cae4bbc4d54941667e
Jordan Dalton <jordan.8474@gmail.com>
2017-09-17 17:42:29 +0100

    feat: add reverse weapon hash file
    Added a file to make it easy to reverse a weapon's hash to get it's name. Also, something for the server owners to mess with f they want :P

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,12 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 client_scripts{
-    "client/client.lua"
+    "client/client.lua",
+    "client/reverse_weapon_hashes.lua"
+}
+
+exports{
+    "reverseWeaponHash"
 }
 
 server_scripts{
