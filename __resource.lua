4cc1a57966fc18ec35c4a2e9ee032f9421aa0e78
Jordan Dalton <jordan.8474@gmail.com>
2017-09-18 19:05:00 +0100

    Update blip generation (Fixes #3)
    Blips are generated from the client so, they're unique to each server :)

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,8 @@ resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 client_scripts{
     "client/client.lua",
-    "client/reverse_weapon_hashes.lua"
+    "client/reverse_weapon_hashes.lua",
+    "client/blips_client.lua"
 }
 
 exports{
@@ -11,5 +12,6 @@ exports{
 
 server_scripts{
     "server/live_map.net.dll",
-    "server/wrapper.lua"
+    "server/wrapper.lua",
+    "server/blips_server.lua"
 }
