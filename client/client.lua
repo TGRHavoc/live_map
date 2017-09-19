69208fb372a5ae651a3bfcb57c95efaf8e2bee7b
Jordan Dalton <jordan.8474@gmail.com>
2017-09-19 16:35:21 +0100

    feat: add vehicle icons
    Player's icon now changes when they enter/exit vehicles.

--- client/client.lua
+++ client/client.lua
@@ -78,8 +78,79 @@ AddEventHandler("playerSpawned", function(spawn)
             GiveWeaponToPed(GetPlayerPed(-1), GetHashKey(w), 1000, 0, false)
         end
     end)
-
 end)
+
+function doVehicleUpdate()
+    local vehicle = GetVehiclePedIsIn(GetPlayerPed(-1), 0)
+
+    if temp["vehicle"] ~= vehicle and vehicle ~= 0 then
+        -- Update it
+        local vehicleClass = GetVehicleClass(vehicle)
+        updateData("Vehicle", GetDisplayNameFromVehicleModel(GetEntityModel(vehicle)))
+        temp["vehicle"] = vehicle
+    end
+
+    local plate = GetVehicleNumberPlateText(vehicle)
+
+    if defaultDataSet["Licence Plate"] ~= plate then
+        Citizen.Trace("Updating plate: " .. plate)
+        updateData("Licence Plate", plate)
+    end
+end
+
+function doIconUpdate()
+    local ped = GetPlayerPed(-1)
+    local newSprite = 6 -- Default to the player one
+
+    if IsEntityDead(ped) then
+        newSprite = 163 -- Using GtaOPassive since I don't have a "death" icon :(
+    else
+        if IsPedSittingInAnyVehicle(ped) then
+            -- Change icon to vehicle
+            -- our temp table should still have the latest vehicle
+            local vehicle = temp["vehicle"]
+            local vehicleModel = GetEntityModel(vehicle)
+            local h = GetHashKey
+
+            if vehicleModel == h("rhino") then
+                newSprite = 421
+            elseif (vehicleModel == h("lazer") or vehicleModel == h("besra") or vehicleModel == h("hydra")) then
+                newSprite = 16 -- Jet
+            elseif IsThisModelAPlane(vehicleModel) then
+                newSprite = 90 -- Airport (plane icon)
+            elseif IsThisModelAHeli(vehicleModel) then
+                newSprite = 64 -- Helicopter
+            elseif (vehicleModel == h("technical") or vehicleModel == h("insurgent") or vehicleModel == h("insurgent2") or vehicleModel == h("limo2")) then
+                newSprite = 426 -- GunCar
+            elseif (vehicleModel == h("dinghy") or vehicleModel == h("dinghy2") or vehicleModel == h("dinghy3")) then
+                newSprite = 404 -- Dinghy
+            elseif (vehicleModel == h("submersible") or vehicleModel == h("submersible2")) then
+                newSprite = 308 -- Sub
+            elseif IsThisModelABoat(vehicleModel) then
+                newSprite = 410
+            elseif (IsThisModelABike(vehicleModel) or IsThisModelABicycle(vehicleModel)) then
+                newSprite = 226
+            elseif (vehicleModel == h("policeold2") or vehicleModel == h("policeold1") or vehicleModel == h("policet") or vehicleModel == h("police") or vehicleModel == h("police2") or vehicleModel == h("police3") or vehicleModel == h("policeb") or vehicleModel == h("riot") or vehicleModel == h("sheriff") or vehicleModel == h("sheriff2") or vehicleModel == h("pranger")) then
+                newSprite = 56 -- PoliceCar
+            elseif vehicleModel == h("taxi") then
+                newSprite = 198
+            elseif (vehicleModel == h("brickade") or vehicleModel == h("stoackage") or vehicleModel == h("stoackage2")) then
+                newSprite = 66 -- ArmoredTruck
+            elseif (vehicleModel == h("towtruck") or vehicleModel == h("towtruck")) then
+                newSprite = 68
+            elseif (vehicleModel == h("trash") or vehicleModel == h("trash2")) then
+                newSprite = 318
+            else
+                newSprite = 225 -- PersonalVehicleCar
+            end
+        end
+    end
+
+    if defaultDataSet["icon"] ~= newSprite then
+        updateData("icon", newSprite)
+    end
+
+end
 
 Citizen.CreateThread(function()
     while true do
@@ -99,7 +170,7 @@ Citizen.CreateThread(function()
 
         -- Update weapons
         local found,weapon = GetCurrentPedWeapon(GetPlayerPed(-1), true)
-        if temp["weapon"] ~= weapon then
+        if found and temp["weapon"] ~= weapon then
             local weaponName = exports[GetCurrentResourceName()]:reverseWeaponHash(tostring(weapon))
             updateData("Weapon", weaponName)
             -- To make sure we don't call this more than we need to
@@ -108,32 +179,19 @@ Citizen.CreateThread(function()
 
         -- Update Vehicle (and icon)
         if IsPedInAnyVehicle(GetPlayerPed(-1)) then
-            local vehicle = GetVehiclePedIsIn(GetPlayerPed(-1), 0)
+            doVehicleUpdate()
 
-            if temp["vehicle"] ~= vehicle and vehicle ~= 0 then
-                -- Update it
-                local vehicleClass = GetVehicleClass(vehicle)
-                updateData("Vehicle", GetDisplayNameFromVehicleModel(GetEntityModel(vehicle)))
-                temp["vehicle"] = vehicle
-            end
-
-            local plate = GetVehicleNumberPlateText(vehicle)
-
-            if defaultDataSet["Licence Plate"] ~= plate then
-                Citizen.Trace("Updating plate: " .. plate)
-                updateData("Licence Plate", plate)
-            end
-
-        else
+        elseif defaultDataSet["Licence Plate"] ~= nil or defaultDataSet["Vehicle"] ~= nil then
             -- No longer in a vehicle, remove "Licence Plate" if present
-            if defaultDataSet["Licence Plate"] ~= nil or defaultDataSet["Vehicle"] ~= nil then
-                defaultDataSet["Licence Plate"] = nil
-                defaultDataSet["Vehicle"] = nil
-                -- Remove it from socket communication
-                TriggerServerEvent("livemap:RemovePlayerData", "Licence Plate")
-                TriggerServerEvent("livemap:RemovePlayerData", "Vehicle")
-            end
+            defaultDataSet["Licence Plate"] = nil
+            defaultDataSet["Vehicle"] = nil
+            temp["vehicle"] = nil
+            -- Remove it from socket communication
+            TriggerServerEvent("livemap:RemovePlayerData", "Licence Plate")
+            TriggerServerEvent("livemap:RemovePlayerData", "Vehicle")
         end
+
+        doIconUpdate()
 
         -- Make sure the updated data is up-to-date on socket server as well
         for i,k in pairs(beenUpdated) do
