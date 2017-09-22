d687c5fb585c5b60e0334e1b30edcdef68629ac7
Jordan Dalton <jordan.8474@gmail.com>
2017-09-22 18:17:25 +0100

    refactor: update server comments
    The server Lua files now have comments and stuff. It's probably not the best but, it'll do.
    
    I'm done for the day.. Time to play some games :D

--- server/wrapper.lua
+++ server/wrapper.lua
@@ -17,17 +17,74 @@ along with this program in the file "LICENSE".  If not, see <http://www.gnu.org/
 ]]
 
 --[[
-    Simple file that wraps all the internal events so that they can just be called
-    by the client without any extra steps needed by the end-developer :)
-]]
+    Adds some data to a player. That will get sent to the UI via websockets
 
+    @param k The name of the data to add to the player (e.g. "Name")
+    @param v The value to set the data to (e.g. "Havoc")
+
+    Note: This MUST be triggered from a client
+]]
 RegisterServerEvent("livemap:AddPlayerData")
+
+--[[
+    Updated some data asssociated with the player
+
+    @param k The name of the data to add to the player (e.g. "Name")
+    @param v The new value to set the data to (e.g. "Some Name")
+
+    Note: This MUST be triggered from a client
+]]
 RegisterServerEvent("livemap:UpdatePlayerData")
 
+--[[
+    Removed some data associated with a player.
+
+    @param k The name of the data to remove from the player (e.g. "Name")
+
+    Note: This MUST be triggered from a client
+]]
 RegisterServerEvent("livemap:RemovePlayerData")
+
+--[[
+    Removes a player from the internal list and notifies any socket clients
+        that said player has left.
+
+    Note: This MUST be triggered from a client
+]]
 RegisterServerEvent("livemap:RemovePlayer")
 
+--[[
+    Associated some static data (data that won't change) with the newly spawned
+        player. This is data that can only be found on the server.
+
+    This adds the player's identifer and their username
+
+    Note: This MUST be triggered from a client
+]]
 RegisterServerEvent("livemap:playerSpawned")
+
+--[[
+    ===================================
+    =           Functions             =
+    ===================================
+]]
+
+--[[
+    Calls internal events (handlers in the DLL) to set some static data
+
+    @param index The server ID of the player (generally source)
+    @param id The first identifer of the player (e.g. "steam:HEX")
+]]
+function setStaticDataFor(index, id)
+    TriggerEvent("livemap:internal_AddPlayerData", id, "identifer", id)
+    TriggerEvent("livemap:internal_AddPlayerData", id, "name", GetPlayerName(index))
+end
+
+--[[
+    ===================================
+    =             Events              =
+    ===================================
+]]
 
 AddEventHandler("livemap:AddPlayerData", function(k, d)
     local id = GetPlayerIdentifier(source, 0) -- Get the first id, it'll do
@@ -57,12 +114,6 @@ AddEventHandler("livemap:RemovePlayer", function()
     TriggerEvent("livemap:internal_RemovePlayer", id)
 end)
 
-
-function setStaticDataFor(index, id)
-    TriggerEvent("livemap:internal_AddPlayerData", id, "identifer", id)
-    TriggerEvent("livemap:internal_AddPlayerData", id, "name", GetPlayerName(index))
-end
-
 --[[
     Set some static data that isn't going to change
     e.g. Player's Identifer
@@ -74,9 +125,10 @@ end)
 
 -- If any players are already on the server
 AddEventHandler("onResourceStart", function(name)
+    print("NUMBER OF PLAYERS: " .. GetNumPlayerIndices())
     if name == GetCurrentResourceName() then
-        if GetNumPlayerIndices() ~= -1 then
-            for i=0, GetNumPlayerIndices()-1 do
+        if GetNumPlayerIndices() ~= 0 then -- There's players on the server :)
+            for i=0, GetNumPlayerIndices() do
                 local id = GetPlayerIdentifier(GetPlayerFromIndex(i), 0)
                 setStaticDataFor(GetPlayerFromIndex(i), id)
             end
