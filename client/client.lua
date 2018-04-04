701ca08dd3182eaac90b067d42d62c593c0e2c7e
Avery Johnson <avery627@gmail.com>
2018-04-03 22:57:40 -0400

    refactor: remove console spam

--- client/client.lua
+++ client/client.lua
@@ -61,7 +61,6 @@ function doVehicleUpdate()
     local plate = GetVehicleNumberPlateText(vehicle)
     plate = plate:gsub("^%s*(.-)%s*$", "%1") -- Remove whitespace at the start and end of the plate (for plate check)
 
-    print("Plate = \"" .. plate .. "\"")
     if plate == "FIVE M" then
         plate = plate .. " (Spawned In)"
     end
