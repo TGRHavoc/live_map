28cd6421bb3b05e21c51124b9ecf36c6b5871fdd
Jordan Dalton <jordan.8474@gmail.com>
2020-04-14 22:03:02 +0100

    feat: update manifest file to latest format

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version "bodacious"
+game "gta5"
+
+author "TGR_Havoc"
+description ""
+version "2.3.2"
+
+--[[ If you want to use the example client, delete this line
+client_script "example_client/*.lua"
+exports {
+    "reverseWeaponHash",
+    "reverseVehicleHash",
+    "reverseStreetHash",
+    "reverseZoneHash",
+    "reverseAreaHash"
+}
+]] -- Remove this line as well
+
+server_scripts{
+    "server/update_check.lua"
+}
