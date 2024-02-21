3a7cc747531e7387962a244d867939aadfa52c88
Jordan Dalton <jordan@yourdsposal.uk>
2024-02-21 18:55:13 +0000

    feat: Add ServiceBuilderExtensions

---
+++ LiveMap/Extensions/ServiceBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using LiveMap.Handlers;
+using LiveMap.Tasks;
+using LiveMap.Utils;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LiveMap.Extensions;
+
+public static class ServiceBuilderExtensions
+{
+    public static IServiceCollection AddSingletonClasses(this IServiceCollection services)
+    {
+        return services.AddSingleton<UpdateChecker>()
+            .AddSingleton<SseService>()
+            .AddSingleton<BlipHandler>()
+            .AddSingleton<BasicHttpHandler>();
+    }
+    
+    public static IServiceCollection AddTasks(this IServiceCollection services)
+    {
+        var taskTypes = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(s => s.GetTypes())
+            .Where(p => typeof(ITask).IsAssignableFrom(p) && !p.IsInterface && !p.IsAbstract);
+        
+        foreach (var taskType in taskTypes)
+        {
+            services.AddSingleton(taskType);
+        }
+        return services;
+    }
+    
+}
