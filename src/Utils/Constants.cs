dd18e7d88fef89eba0e49ecceb5c08af4a99c788
Jordan Dalton <jordan@yourdsposal.uk>
2024-02-21 09:02:24 +0000

    feat: Add constants class

---
+++ src/Utils/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace LiveMap.Utils
+{
+    public static class Constants
+    {
+        public static class Config
+        {
+            public static readonly string Debug = "livemap_debug";
+            public static readonly string BlipFile = "livemap_blipFile";
+        }
+        
+        public static readonly string DefaultBlipFile = "blips.json";
+        
+        public static readonly string LiveMapUpdateUrl = "https://api.github.com/repos/TGRHavoc/live_map/releases/latest";
+        public static readonly Version LiveMapVersion = Assembly.GetExecutingAssembly().GetName().Version;
+    }
+}
