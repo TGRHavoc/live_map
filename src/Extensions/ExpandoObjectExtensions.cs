1ba660447c0f3d115c937e4c18baf55358d977d7
Jordan Dalton <jordan@yourdsposal.uk>
2024-02-21 09:27:41 +0000

    feat(Extensions): Add FromExpando
    Will take an ExpandoObject and create a new object, mapping all Expando properties to the same properties in the class used for the new object

---
+++ src/Extensions/ExpandoObjectExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json.Serialization;
+using CitizenFX.Core;
+
+namespace LiveMap.Extensions
+{
+    public static class ExpandoObjectExtensions
+    {
+        public static T FromExpando<T>(this ExpandoObject expando) where T : class, new()
+        {
+            if (expando == null) return null;
+
+            var properties = typeof(T)
+                .GetProperties()
+                .Where(pi => pi.CanWrite && !pi.GetIndexParameters().Any())
+                // Use the json property name if it exists, otherwise use the property name
+                .ToDictionary(pi => pi.GetCustomAttribute<JsonPropertyNameAttribute>()?.Name?.ToLower() ?? pi.Name.ToLower());
+            
+            Debug.WriteLine("Properties: " + properties.Count);
+
+            T obj = new T();
+            foreach (var kvp in expando)
+            {
+                var name = kvp.Key.ToLower().Replace("_", "");
+                var val = kvp.Value;
+                Debug.WriteLine($"Property: {name} Value: {val}");
+                
+                if (val != null && properties.TryGetValue(name, out PropertyInfo prop))
+                {
+                    if (prop.PropertyType.IsAssignableFrom(val.GetType()))
+                    {
+                        prop.SetValue(obj, val);
+                        Debug.WriteLine($"Set property: {name} to {val}");
+                    }
+                }
+            }
+            return obj;
+        }
+    }
+}
