00929a559f7f8527df61515c3c9536dc99a9cf67
Jordan Dalton <jordan@yourdsposal.uk>
2024-02-21 18:58:34 +0000

    feat: Add SseService
    Will allow us to do sse stuff via di

---
+++ LiveMap/Utils/SseService.cs
@@ -0,0 +1,44 @@
+﻿using LiveMap.Extensions;
+using LiveMap.Models;
+
+namespace LiveMap.Utils
+{
+    public class SseService
+    {
+        // This is a simple class that will be used to send Server-Sent Events to the client
+
+        private readonly List<HttpResponse> _openResponses = new List<HttpResponse>();
+
+        public SseService()
+        {
+        }
+        
+        public void AddResponse(HttpResponse response)
+        {
+            _openResponses.Add(response);
+            response.SendEvent("connected", new {Connected = true});
+        }
+        
+        public void RemoveResponse(HttpResponse response)
+        {
+            _openResponses.Remove(response);
+        }
+        
+        public void BroadcastEvent(string eventName, object data)
+        {
+            foreach (var response in _openResponses)
+            {
+                response.SendEvent(eventName, data);
+            }
+        }
+
+        public async Task KeepAlivePing()
+        {
+            foreach (var response in _openResponses)
+            {
+                response.Write(":keep-alive\n\n");
+            }
+        }
+        
+    }
+}
