1144ac2d91c2f8ef20e29f04e99249982ee967fb
Jordan Dalton <jordan@yourdsposal.uk>
2024-02-21 18:59:08 +0000

    feat: Add KeepSseAlive task
    Will send a comment every couple of seconds to keep connections alive as per the rfc

---
+++ LiveMap/Extensions/HttpResponseExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+using LiveMap.Models;
+
+namespace LiveMap.Extensions
+{
+    public static class HttpResponseExtensions
+    {
+        
+        public static void SendEvent(this HttpResponse response, string eventName, object data)
+        {
+            response.Write("event: " + eventName + "\n");
+            response.Write("data: " + JsonSerializer.Serialize(data) + "\n\n");
+        }
+        
+    }
+}
---
+++ LiveMap/Tasks/KeepSseAlive.cs
@@ -0,0 +1,29 @@
+﻿using CitizenFX.Core;
+using LiveMap.Utils;
+using Microsoft.Extensions.Logging;
+
+namespace LiveMap.Tasks
+{
+    public class KeepSseAlive : ITask
+    {
+        private readonly SseService _sseService;
+        private readonly ILogger<KeepSseAlive> _logger;
+        public KeepSseAlive(SseService sseService, ILogger<KeepSseAlive> logger)
+        {
+            _sseService = sseService;
+            _logger = logger;
+            // Tick += Execute;
+        }
+
+        public async Coroutine Execute()
+        {
+            await Coroutine.Delay(10_000);
+            await _sseService.KeepAlivePing();
+        }
+    }
+
+    public interface ITask
+    {
+        Coroutine Execute();
+    }
+}
