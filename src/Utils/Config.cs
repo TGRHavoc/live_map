74426eae9f090374b1c662839e3b4bd55f70a74c
Jordan Dalton <jordan@yourdsposal.uk>
2024-02-21 09:26:07 +0000

    feat(Utils): Add config class
    Used to get config values for livemap

---
+++ src/Utils/Config.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using CitizenFX.Core.Native;
+using Microsoft.Extensions.Logging;
+
+namespace LiveMap.Utils
+{
+    public static class Config
+    {
+        public static T GetConfigKeyValue<T>(string metadataKey, int index, T defaultValue, ILogger logger = null)
+        {
+            var result = defaultValue;
+            
+            try
+            {
+                var input = API.GetResourceMetadata(LiveMap.ResourceName, metadataKey, index);
+                result = (T) TypeDescriptor.GetConverter(typeof(T)).ConvertFromString(input);
+            }
+            catch (Exception)
+            {
+                logger?.LogError("Failed to parse {MetadataKey}. Using default value {DefaultValue}", metadataKey, defaultValue);
+            }
+
+            return result;
+        }
+    }
+}
